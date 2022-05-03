52bfd4337d89aec7ac492b50518f463ffd64569f
Evanflow89 <ivanbaragone@gmail.com>
2022-05-03 15:09:38 +0200

    Aggiunte possibilità vittoria o pareggio

--- dadi/main.js
+++ dadi/main.js
@@ -3,4 +3,12 @@
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 const rollDie1 = Math.floor((Math.random() * 6) + 1);
-console.log(rollDie1);
+const rollDie2 = Math.floor((Math.random() * 6) + 1);
+
+if (rollDie1 > rollDie2) {
+    console.log(`Il giocatore 1 ha fatto ${rollDie1}, il giocatore 2 ha fatto ${rollDie2}, il giocatore 1 ha vinto!`);
+} else if (rollDie1 < rollDie2) {
+    console.log(`Il giocatore 1 ha fatto ${rollDie1}, il giocatore 2 ha fatto ${rollDie2}, il giocatore 2 ha vinto!`);
+} else if (rollDie1 === rollDie2) {
+    console.log(`Il giocatore 1 ha fatto ${rollDie1}, il giocatore 2 ha fatto ${rollDie2}, pareggio!`);
+}
