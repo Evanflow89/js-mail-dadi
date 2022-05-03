af28b986748836f310a0b80e2e8121f307d84c70
Evanflow89 <ivanbaragone@gmail.com>
2022-05-03 15:15:50 +0200

    Added an array of emails

---
+++ mail/main.js
@@ -0,0 +1,8 @@
+// Mail
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+
+
+const eMail = ["pino@tiscali.it", "lino@tiscali.it", "dino@tiscali.it", "gino@tiscali.it", "tino@tiscali.it"];
